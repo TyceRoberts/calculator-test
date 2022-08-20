9cef600d0363a9e0fb262055058d3ef57ac23750
Tyce <tycedylan9@gmail.com>
2022-08-20 14:06:08 -0600

    Built out calc functionality

--- calc.js
+++ calc.js
@@ -1,18 +1,36 @@
 const readline = require('readline');
 
 const reader = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
+	input: process.stdin,
+	output: process.stdout,
 });
 
 
 
-reader.question("What would you like to calculate?", function(input){
+reader.question("What would you like to calculate?", function (input) {
 	const tokens = input.split(' ');
-	
+
 	const mathSymbol = tokens[0];
 	const num1 = Number(tokens[1]);
 	const num2 = Number(tokens[2]);
+
+	console.log(mathSymbol);
+	console.log(num1);
+	console.log(num2);
+
+	if (mathSymbol === '+') {
+		let sum = num1 + num2;
+		console.log(sum);
+	} else if (mathSymbol === '-') {
+		let sum = num1 - num2;
+		console.log(sum);
+	} else if (mathSymbol === '*') {
+		let sum = num1 * num2;
+		console.log(sum);
+	} else if (mathSymbol === '/') {
+		let sum = num1 / num2;
+		console.log(sum);
+	}
 
 
 	// This line closes the connection to the command line interface.
